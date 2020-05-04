e28f4eb2ff23e8797b6b30cee59469094eae3055
akrinos <akrinos@vt.edu>
2020-05-04 09:11:09 -0400

    Create samplescript.py

---
+++ scripts/samplescript.py
@@ -0,0 +1,8 @@
+# You'll want a folder to put your code, so that you can all collaborate. Make sure that if you're 
+# working on the same scripts, you pay special attention to version management! Reach out to Arianna or your
+# mentor if you don't have much experience with this.
+
+import os
+import sys
+
+print("Hello, world!")
